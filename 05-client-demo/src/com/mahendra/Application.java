a78e82af5ea1b47a9b84a244c85fa2c41d5488c1
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2023-03-28 17:03:41 +0530

    Client Demos

---
+++ 05-client-demo/src/com/mahendra/Application.java
@@ -0,0 +1,27 @@
+package com.mahendra;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
+
+import jakarta.ws.rs.client.*;
+import jakarta.ws.rs.core.MediaType;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// Lightweight HTTP Client
+		Client client = ClientBuilder.newClient().register(new JacksonJaxbJsonProvider());
+		/*
+		 * Response resp =
+		 * client.target("http://localhost:8090/02-demo/prefix/customers")
+		 * .request(MediaType.APPLICATION_JSON) .buildGet() .invoke();
+		 * System.out.println("Response Status : "+ resp.getStatus());
+		 * System.out.println("Response entity : "+resp.readEntity(String.class));
+		 */
+		Customer c = client.target("http://localhost:8090/02-demo/prefix/customers/1")
+					.request().accept(MediaType.APPLICATION_JSON).get(Customer.class);
+					
+					
+		System.out.println(c.getCustomerId()+" "+c.getFirstName()+" "+c.getLastName());
+		
+	}
+}
---
+++ 06-client/src/main/java/com/mahendra/Customer.java
@@ -0,0 +1,44 @@
+package com.mahendra;
+
+public class Customer {
+	private String customerId;
+	private String firstName;
+	private String lastName;
+	private String phone;
+	public Customer(String customerId, String firstName, String lastName, String phone) {
+		super();
+		this.customerId = customerId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phone = phone;
+	}
+	public Customer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public String getCustomerId() {
+		return customerId;
+	}
+	public void setCustomerId(String customerId) {
+		this.customerId = customerId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+}
